87f26024d1c8d10b07f3ead05f613688d1a51e98
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-02-15 08:43:12 -0800

    Initial Commit

---
+++ GameKitTest/SFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SFAppDelegate.h
+//  GameKitTest
+//
+//  Created by Spencer Fornaciari on 2/15/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GameKitTest/SFViewController.h
@@ -0,0 +1,13 @@
+//
+//  SFViewController.h
+//  GameKitTest
+//
+//  Created by Spencer Fornaciari on 2/15/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SFViewController : UIViewController
+
+@end
