032badcbd9603edc9ead8030d15c2770fd788751
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-02-15 08:57:58 -0800

    Testing with Gamekit for Swing Boxer

---
+++ MatchmakingServer.m
@@ -0,0 +1,14 @@
+//
+//  MatchmakingServer.m
+//  GameKitTest
+//
+//  Created by Spencer Fornaciari on 2/15/14.
+//  Copyright (c) 2014 Spencer Fornaciari. All rights reserved.
+//
+
+#import "MatchmakingServer.h"
+
+@implementation MatchmakingServer
+
+
+@end
